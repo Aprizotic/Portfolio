8d911a1528db0ef8998801b97d38acc3a865d65f
Aprizotic <kajusbut24@gmail.com>
2026-01-10 17:03:43 +0000

    finish history section

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
 import './css/main.scss';
 
+const showEducationButton = document.querySelector('[data-js="show-education"]');
+const showExperienceButton = document.querySelector('[data-js="show-experience"]');
+const educationSection = document.querySelector('.main__section--education');
+const experienceSection = document.querySelector('.main__section--experience');
+
+function toggleHistory() {
+  educationSection.classList.toggle('hidden');
+  experienceSection.classList.toggle('hidden');
+  showExperienceButton.classList.toggle('button--selected');
+  showEducationButton.classList.toggle('button--selected');
+}
+
+showExperienceButton.addEventListener('click', toggleHistory);
+showEducationButton.addEventListener('click', toggleHistory);
