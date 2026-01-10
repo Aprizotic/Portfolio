ebd51788e18bf2a3353649867393acf251ec7876
Aprizotic <kajusbut24@gmail.com>
2026-01-10 20:05:32 +0000

    finish base design and font

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './css/main.scss';
 
 const showEducationButton = document.querySelector('[data-js="show-education"]');
 const showExperienceButton = document.querySelector('[data-js="show-experience"]');
+const jumpButton = document.querySelector('[data-js="jump-button"]');
 const educationSection = document.querySelector('.main__section--education');
 const experienceSection = document.querySelector('.main__section--experience');
 
@@ -14,3 +15,16 @@ function toggleHistory() {
 
 showExperienceButton.addEventListener('click', toggleHistory);
 showEducationButton.addEventListener('click', toggleHistory);
+
+window.addEventListener('pageshow', (event) => {
+  if (event.persisted) {
+    window.location.reload();
+  }
+});
+
+jumpButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
